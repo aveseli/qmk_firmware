6db47132b193782658b20a4a96563a0a1dce37df
aveseli <adnan.veseli@gmail.com>
2023-01-27 22:02:18 +0100

    right control

--- keyboards/lily58/keymaps/aveseli/keymap.c
+++ keyboards/lily58/keymaps/aveseli/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  DEL  |    |  BSPC |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LAlt |Space | / LOWER /       \RAISE \  |Enter | RAlt | ESC  |
+ *                   | LGUI | LAlt |Space | / LOWER /       \RAISE \  |Enter | RAlt | RCTL |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P, KC_EQL,
   CTL_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
   KC_LSFT,       KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_DEL,  KC_BSPC,   KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,
-                                KC_LGUI, KC_LALT, KC_SPC,     LOWER,  RAISE,        KC_ENT,  KC_RALT, KC_ESC
+                                KC_LGUI, KC_LALT, KC_SPC,     LOWER,  RAISE,        KC_ENT,  KC_RALT, KC_RCTL
 ),
 
 
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
   KC_LCTL, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
   _______,  _______, _______, _______,_______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+                             _______, _______, _______, _______, _______,  _______, _______, KC_RCTL
 ),
 
 /* MAC
